6a4417e7a99e686a3a001425f17cabc00011f5ae
Maes95 <michel.maes95@gmail.com>
2021-01-07 20:53:28 +0100

    New example of WebClient in Spring

---
+++ tema2/webclient_ejem1/src/test/java/es/codeurjc/test/webclient_ejem1/WebclientMockMVCTests.java
@@ -0,0 +1,53 @@
+package es.codeurjc.test.webclient_ejem1;
+
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.client.MockMvcWebTestClient;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class WebclientMockMVCTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private BooksService booksService;
+
+    private WebTestClient webTestClient;
+
+    @BeforeEach
+    public void setup() {
+        this.webTestClient = MockMvcWebTestClient
+            .bindTo(mockMvc)
+            .build();
+
+        when(booksService.getBookTitles("Java"))
+            .thenReturn(List.of("Java SE 6","Java a Tope: J2me (java 2 Micro Edition).","Manual de Java","Java API FAQ"));
+    }
+
+    @Test
+	public void MOCK_getAllItemsTest() throws InterruptedException {
+		this.webTestClient
+			.get()
+				.uri("/booktitles?title=Java")
+			.exchange()
+				.expectStatus()
+					.isOk()
+				.expectBody()
+					.jsonPath("$[0]")
+						.value(Matchers.containsString("Java"));
+	}
+    
+}
---
+++ tema2/webclient_ejem1/src/test/java/es/codeurjc/test/webclient_ejem1/WebclientRESTTests.java
@@ -0,0 +1,29 @@
+package es.codeurjc.test.webclient_ejem1;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class WebclientRESTTests {
+
+	@Autowired
+	private WebTestClient webTestClient;
+
+	@Test
+	public void REST_getAllItemsTest() throws InterruptedException {
+		this.webTestClient
+			.get()
+				.uri("/booktitles?title=Java")
+			.exchange()
+				.expectStatus()
+					.isOk()
+				.expectBody()
+					.jsonPath("$[0]")
+						.value(Matchers.containsString("Java"));
+	}
+
+}
