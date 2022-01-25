3f1f1482ec340682f4c93d5864d3ec360b2b5db3
Maes95 <michel.maes95@gmail.com>
2022-01-26 00:36:20 +0100

    Add tema3 changes

--- tema3/jest-ejem1/src/calculadora.js
+++ tema3/jest-ejem1/src/calculadora.js
@@ -1,10 +1,10 @@
 module.exports = class Calculadora {
 
-	suma(op1, op2) {
-		return op1 + op2;
-	}
+    suma(op1, op2) {
+        return op1 + op2;
+    }
 
-	resta(op1, op2) {
-		return op1 - op2;
-	}
+    resta(op1, op2) {
+        return op1 - op2;
+    }
 }
