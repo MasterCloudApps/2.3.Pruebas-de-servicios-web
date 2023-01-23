ff0b5b7cab82af0f77927adaaa7d7d95298378ca
Maes95 <michel.maes95@gmail.com>
2023-01-23 21:26:55 +0100

    Fix atdd_ejer3

--- tema6/atdd_ejer3_solucion/src/test/java/es/codeurjc/anuncios/AnunciosControllerTest.java
+++ tema6/atdd_ejer3_solucion/src/test/java/es/codeurjc/anuncios/AnunciosControllerTest.java
@@ -1,8 +1,8 @@
 package es.codeurjc.anuncios;
 
-import org.springframework.boot.test.context.SpringBootTest;
-
 import com.intuit.karate.junit5.Karate;
+
+import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class AnunciosControllerTest {
